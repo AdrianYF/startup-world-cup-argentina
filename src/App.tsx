78d390c48726603ebcdb327980fccfdb3b5faa48
Adrian <vilasadrian@gmail.com>
2026-05-23 13:28:38 -0300

    primer commit - base del proyecto

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <h1 className="text-4xl font-bold text-center py-10">
+        Startup World Cup Argentina 2026
+      </h1>
+    </div>
+  )
+}
+
+export default App
