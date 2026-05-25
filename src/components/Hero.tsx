0d80a699fd229dbd0f9a78d718970aacc92d9962
sebastianlujan <glujan.recalde@gmail.com>
2026-05-25 11:08:58 -0300

    Revert "feat(hero): trofeo dorado Pegasus como protagonista + cleanup palette"
    This reverts commit 91240fda25cdd74f12a70a3534b962de7e6ed39a.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { content } from '../lib/content'
 import { openStartupForm, openPartnerForm } from '../lib/ticketing'
+import { GradientText } from './ui/GradientText'
 
 function Hero() {
   const [timeLeft, setTimeLeft] = useState({ dias: 0, horas: 0, mins: 0, segs: 0 })
@@ -60,9 +61,9 @@ function Hero() {
                 <div className="w-3/4 h-3/4 bg-gradient-to-br from-[#75AADB]/30 via-[#75AADB]/15 to-transparent blur-3xl rounded-full" />
               </div>
               <img
-                src="/gold.png"
+                src="/SWC-logo.png"
                 alt="Startup World Cup"
-                className="relative h-44 sm:h-52 md:h-56 lg:h-64 xl:h-80 w-auto drop-shadow-[0_12px_48px_rgba(234,179,8,0.55)]"
+                className="relative h-40 sm:h-48 md:h-52 lg:h-60 xl:h-72 w-auto drop-shadow-[0_12px_48px_rgba(108,92,231,0.5)]"
               />
             </div>
 
@@ -98,7 +99,7 @@ function Hero() {
               {/* H1 */}
               <h1 className="text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-black leading-[1.05] uppercase tracking-wide mb-6 lg:mb-8">
                 <span className="block text-white">STARTUP WORLD CUP</span>
-                <span className="block text-[#75AADB]">ARGENTINA</span>
+                <GradientText as="span" className="block">ARGENTINA</GradientText>
               </h1>
 
               {/* Tagline */}
