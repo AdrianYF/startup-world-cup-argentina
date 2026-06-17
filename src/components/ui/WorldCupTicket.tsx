3aea0c680a3720de18c5713fd45b61339bc5b035
sebastianlujan <glujan.recalde@gmail.com>
2026-06-16 23:16:51 -0300

    style(ticket): vuelve los semicírculos a negro

--- src/components/ui/WorldCupTicket.tsx
+++ src/components/ui/WorldCupTicket.tsx
@@ -282,8 +282,8 @@ function FullTicket({ formatUSD }: { formatUSD: string }) {
             className="relative w-4 sm:w-6 border-l-2 border-dashed border-[#0f172b]/25 flex-shrink-0"
             style={{ backgroundImage: 'linear-gradient(180deg, #4F46E5 0%, #6c5ce7 30%, #c084fc 60%, #ff7675 100%)' }}
           >
-            <div className="absolute -top-1.5 sm:-top-2.5 left-1/2 -translate-x-1/2 w-3 h-3 sm:w-5 sm:h-5 bg-white rounded-full z-10" />
-            <div className="absolute -bottom-1.5 sm:-bottom-2.5 left-1/2 -translate-x-1/2 w-3 h-3 sm:w-5 sm:h-5 bg-white rounded-full z-10" />
+            <div className="absolute -top-1.5 sm:-top-2.5 left-1/2 -translate-x-1/2 w-3 h-3 sm:w-5 sm:h-5 bg-[#0f172b] rounded-full z-10" />
+            <div className="absolute -bottom-1.5 sm:-bottom-2.5 left-1/2 -translate-x-1/2 w-3 h-3 sm:w-5 sm:h-5 bg-[#0f172b] rounded-full z-10" />
 
             {/* Hint de interacción: texto rotado 90° sobre la perforación (desaparece al sellar) */}
             {!stamped && (
