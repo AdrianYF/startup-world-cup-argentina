84f1c32182459396ce830c769003f462e4829349
sebastianlujan <glujan.recalde@gmail.com>
2026-06-23 12:59:59 -0300

    feat(galeria): mover la sección Galería justo después de Agenda

--- src/App.tsx
+++ src/App.tsx
@@ -40,6 +40,7 @@ function App() {
   const midSections = [
     PitchBattle,
     Agenda,
+    Galeria,
     Tickets,
     Startups,
     Voluntarios,
@@ -77,9 +78,6 @@ function App() {
             <Section />
           </FadeInSection>
         ))}
-        <FadeInSection>
-          <Galeria />
-        </FadeInSection>
       </main>
       <FadeInSection>
         <Footer />
